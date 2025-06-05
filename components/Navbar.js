f48e401b6bfd29f1902c296b32fc9b5e7024d447
gabinhallosserie <gabinhalloss@gmail.com>
2025-06-05 02:05:02 +0200

    v1.0

---
+++ components/Navbar.js
@@ -0,0 +1,60 @@
+"use client";
+
+import {useEffect, useState} from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+export default function Navbar() {
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setIsScrolled(window.scrollY > 50);
+        };
+
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
+
+    return (
+        <div id={"container"}
+             className={`sticky top-0 ${isScrolled ? "w-full h-20 border-b" : "w-2/3 h-16 border"} px-6 bg-white/60 backdrop-blur-md text-black mt-4 flex justify-between items-center ${isScrolled ? "" : "rounded-full"} transition-all duration-300 ease-in-out z-20`}>
+            <div className={"flex items-center space-x-3"}>
+                <Link href={"/public"} className={"flex justify-start items-center gap-3"}>
+                    {isScrolled && (
+                        <Image src={"/assets/profile.JPEG"} width={1920} height={1920} alt={"Gabin Hallosserie"}
+                               className={"w-12 h-12 rounded-full transition-transform duration-300"}/>
+                    )}
+                    <div className={"flex flex-col items-start"}>
+                        <Image src={"/assets/profile.JPEG"} width={1920} height={1920} alt={"Gabin Hallosserie"}
+                               className={`block md:hidden w-12 h-12 rounded-xl ${isScrolled ? "hidden" : "block"} transition-transform duration-300`}/>
+                        <h1 className={`hidden md:block text-lg font-semibold transition-all duration-300 ${isScrolled ? "-mt-1" : ""}`}>Gabin
+                            Hallosserie</h1>
+                        <p className={"hidden md:block text-xs text-gray-600"}>Développeur web junior</p>
+                    </div>
+                </Link>
+            </div>
+
+            <div className={"flex items-center gap-8"}>
+                <Link href={"#about"}
+                      className={"relative text-sm font-semibold transition-all duration-300 ease-in-out group"}>
+                    À propos
+                    <span
+                        className={"absolute left-0 bottom-0 w-0 h-0.5 bg-black transition-all duration-300 group-hover:w-full"}></span>
+                </Link>
+                <Link href={"#projects"}
+                      className={"relative text-sm font-semibold transition-all duration-300 ease-in-out group"}>
+                    Projets
+                    <span
+                        className={"absolute left-0 bottom-0 w-0 h-0.5 bg-black transition-all duration-300 group-hover:w-full"}></span>
+                </Link>
+                <Link href={"#contact"}
+                      className={"relative text-sm font-semibold transition-all duration-300 ease-in-out group"}>
+                    Contact
+                    <span
+                        className={"absolute left-0 bottom-0 w-0 h-0.5 bg-black transition-all duration-300 group-hover:w-full"}></span>
+                </Link>
+            </div>
+        </div>
+    );
+}
---
+++ components/Projects.js
@@ -0,0 +1,61 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Projects() {
+    const projects = [
+        {
+            name: "NeuroForge",
+            description: "NeuroForge développe des solutions d’IA embarquée pour optimiser les performances des objets connectés industriels.",
+            link: "",
+            logo: "/assets/logos/webstorm.svg",
+            date: "juin 2025 - en cours de développement"
+        },
+        {
+            name: "Bofar - Restaurant savoyard",
+            description: "Le Bofar est un site vitrine pour un restaurant fictif typique savoyard, permettant de découvrir le restaurant et de réserver une table.",
+            link: "",
+            logo: "/assets/logos/webstorm.svg",
+            date: "juin 2024 - juillet 2024"
+        },
+        {
+            name: "Nexus - Social Network",
+            description: "Nexus est une application mobile de type reseau social permettant de mettre en lien les joueurs de jeux vidéos en fonction de leurs jeux préférés et de leur niveau de jeu.",
+            link: "",
+            logo: "/assets/logos/webstorm.svg",
+            date: "juin 2024 - juillet 2024"
+        },
+    ]
+
+    return (
+        <div id={"projects"} className={"w-2/3 flex flex-col justify-start items-start gap-10 mx-auto mb-20"}>
+            <div className={"w-full grid grid-cols-1 md:grid-cols-2 gap-10"}>
+                {projects.map((project, index) => (
+                    <div
+                        className={"flex flex-col items-start border border-gray-500 rounded-md p-5 bg-white shadow-sm hover:shadow-lg transition-all duration-300 ease-in-out"}
+                        key={index}>
+                        <div className={"w-full flex justify-between items-center"}>
+                            <div className={"flex flex-col justify-start items-start"}>
+                                <p className={"font-bold text-md"}>{project.name}</p>
+                                {project.link.trim() !== "" ? (
+                                    <Link href={project.link} target="_blank"
+                                          className="font-light text-xs italic underline">{project.link}</Link>
+                                ) : (
+                                    <span className="font-light text-xs italic">Projet privé ou indisponible</span>
+                                )}
+                            </div>
+                            <Image src={project.logo} alt="Projet logo" width={30} height={30}/>
+                        </div>
+
+                        <p className={"text-sm font-medium mt-5 line-clamp-3"}>{project.description}</p>
+
+                        <hr className={"w-full h-px bg-gray-300 mt-5"}/>
+
+                        <div className={"w-full flex justify-between items-center mt-5"}>
+                            <p className={"text-xs font-light text-gray-500"}>{project.date}</p>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
